68173d76319e0ff6e9cd26e8255585c7c0d0427c
= <=>
2021-09-22 01:39:56 -0500

    Se añade secciones

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// let btn_normas = document.querySelector('.btn_normas');
+// let estandares = document.querySelector('.estandares');
+
+// let coords = estandares.getBoundingClientRect();
+
+// console.log(coords.top);
+
+// btn_normas.addEventListener('click', ()=> {
+//     window.scrollTo({
+//         top: coords.top
+//     });
+// });
+
+let normas = document.querySelector('.normas');
+// const pos = document.documentElement.getBoundingClientRect();
+// const pos_normas = normas.getBoundingClientRect();
+
+// let pos = normas.offsetTop;
+
+// let center = document.documentElement.getBoundingClientRect();
+
+// console.log(pos, center.top / -1);
+
+// while (center.top / -1 >= pos){
+//     normas.style.display ="none";
+// }
+// while(pos.x >= pos_normas.x) {
+//     normas.styles.background ='red';
+//     console.log('asfasf')
+// }
