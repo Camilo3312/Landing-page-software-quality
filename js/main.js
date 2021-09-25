d2b39ce223f3e44133e1f190f383975348324b62
camilo3312 <ccvelasquez531@misena.edu.co>
2021-09-25 21:39:46 +0200

    Se añade JS

--- js/main.js
+++ js/main.js
@@ -1,82 +1,33 @@
-// $(document).ready(function(){
-
-//     $('.btn_normas').click(function(){
-//         var pos = $('.iso').position();
-//         console.log(pos.top)
-// 		$('body, html').animate({
-// 			scrollTop: pos.top 
-// 		}, 200);
-// 	});
-	
-// 	$('.btn_iso').click(function(){
-//         var pos = $('.iso').position();
-//         console.log(pos.top)
-// 		$('body, html').animate({
-// 			scrollTop: pos.top 
-// 		}, 200);
-// 	});
-	
-// 	$('.btn_spice').click(function(){
-//         var pos = $('.spice').position();
-//         console.log(pos.top)
-// 		$('body, html').animate({
-// 			scrollTop: pos.top 
-// 		}, 200);
-// 	});
-	
-// 	$('.btn_cmmi').click(function(){
-//         var pos = $('.cmmi').position();
-//         console.log(pos.top)
-// 		$('body, html').animate({
-// 			scrollTop: pos.top 
-// 		}, 200);
-// 	});
-	
-// 	$('.btn_psp').click(function(){
-//         var pos = $('.psp').position();
-//         console.log(pos.top)
-// 		$('body, html').animate({
-// 			scrollTop: pos.top 
-// 		}, 200);
-// 	});
-	
-// });
 let elemets = document.querySelectorAll('.btn_normas');
-const arr = Array(elemets);
+let info = document.querySelectorAll('.info');
+let btn_irnormas = document.querySelector('.btn_irnormas');
 
-arr.forEach(element => {
-	element.addEventListener('click', ()=> {
-		
-	});
+const arr = Array.from(elemets);
+const arrinfo = Array.from(info);
+
+function posInfo(e){
+	let pos = arr.indexOf(e);
+	const info = arrinfo[pos];
+	const pos_info = info.offsetTop;
+	return pos_info;
+}
+
+btn_irnormas.addEventListener('click', () => {
+	const posFirstInfo = arrinfo[0];
+	
+	window.scroll({
+		top: posFirstInfo.offsetTop - 85,
+		behavior: 'smooth'
+	});	
+	
 });
 
-// let btn_normas = document.querySelector('.btn_normas');
-// let estandares = document.querySelector('.estandares');
-
-// let coords = estandares.getBoundingClientRect();
-
-// console.log(coords.top);
-
-// btn_normas.addEventListener('click', ()=> {
-//     window.scrollTo({
-//         top: coords.top
-//     });
-// });
-
-// let normas = document.querySelector('.normas');
-// const pos = document.documentElement.getBoundingClientRect();
-// const pos_normas = normas.getBoundingClientRect();
-
-// let pos = normas.offsetTop;
-
-// let center = document.documentElement.getBoundingClientRect();
-
-// console.log(pos, center.top / -1);
-
-// while (center.top / -1 >= pos){
-//     normas.style.display ="none";
-// }
-// while(pos.x >= pos_normas.x) {
-//     normas.styles.background ='red';
-//     console.log('asfasf')
-// }
+arr.forEach(e => {
+	e.addEventListener('click', () => {
+		let pos_info = posInfo(e);
+		window.scroll({
+			top: pos_info - 85,
+			behavior: 'smooth'
+		});			
+	});
+});
