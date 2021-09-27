377b004d334fd89a62605ee7565ac04104f156cc
camilo3312 <ccvelasquez531@misena.edu.co>
2021-09-27 09:44:31 +0200

    Se añade responsive

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
 let elemets = document.querySelectorAll('.btn_normas');
 let info = document.querySelectorAll('.info');
 let btn_irnormas = document.querySelector('.btn_irnormas');
+let nombre = document.querySelector('.name');
+let message = document.querySelector('.message');
+let btn_comment = document.querySelector('.btn_comentario');
 
+var messagesJson = JSON.stringify();
 const arr = Array.from(elemets);
 const arrinfo = Array.from(info);
+
+// Messages
+
+btn_comment.addEventListener('click', () => {
+	let date = new Date();
+	localStorage.setItem("Nombre",nombre.value);
+	localStorage.setItem('Fecha', date);
+	localStorage.setItem("Nombre",menssage.value);
+
+	let peticion = localStorage.getItem("Nombre");
+	console.log(peticion)
+});
+
+// Nav normas
 
 function posInfo(e){
 	let pos = arr.indexOf(e);
@@ -29,3 +47,4 @@ arr.forEach(e => {
 		});			
 	});
 });
+
